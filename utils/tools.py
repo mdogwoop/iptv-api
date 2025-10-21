a2c247fe79e97197c6314fc80bf705d9738e5f9b
ldm2060 <ldm200026@live.cn>
2025-10-21 21:11:02 +0800

    Add webview demo M3U file and update EPG URL in tools.py

--- utils/tools.py
+++ utils/tools.py
@@ -348,7 +348,7 @@ def convert_to_m3u(path=None, first_channel_name=None, data=None):
     """
     if os.path.exists(path):
         with open(path, "r", encoding="utf-8") as file:
-            m3u_output = f'#EXTM3U x-tvg-url="{get_epg_url()}"\n'
+            m3u_output = f'#EXTM3U x-tvg-url="https://gitee.com/mytv-android/myepg/raw/master/output/epg.gz"\n'#"{get_epg_url()}"\n'
             current_group = None
             logo_url = join_url(config.cdn_url,
                                 config.logo_url) if "raw.githubusercontent.com" in config.logo_url else config.logo_url
